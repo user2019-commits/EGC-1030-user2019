31367d1f45f27ab07f9569a9764fff69bafc718f
YassinETSII <mohlalela@us.es>
2019-09-08 17:00:48 +0200

    probando

--- decide/local_settings.travis.py
+++ decide/local_settings.travis.py
@@ -31,8 +31,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
-        'USER': 'decide',
-	   'PASSWORD':'decide',
+        'USER': 'decide'
+        'PASSWORD':'decide',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
