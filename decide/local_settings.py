3181fedfaa3e3be5f83946b184bc227dbd680396
YassinETSII <mohlalela@us.es>
2019-09-08 17:28:36 +0200

    probando again

--- decide/local_settings.py
+++ decide/local_settings.py
@@ -29,12 +29,12 @@ BASEURL = 'http://localhost:8000'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
-    }
+'ENGINE': 'django.db.backends.postgresql',
+'NAME': 'postgres',
+'USER': 'decide',
+'PASSWORD': 'decide',
+'HOST': '127.0.0.1', 
+'PORT': '5432', }
 }
 
 # number of bits for the key, all auths should use the same number of bits
